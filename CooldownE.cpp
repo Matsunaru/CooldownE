f8cae427ac1b7418841470350cd695702b11fa42
Matsunaru <error1998pl@gmail.com>
2024-11-16 05:30:09 +0100

    B

--- CooldownE.cpp
+++ CooldownE.cpp
@@ -1,20 +1,70 @@
-﻿// CooldownE.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <windows.h>
+#include <thread>
+using namespace std;
 
-#include <iostream>
+bool insAndDownPressed = false;
+
+void setCursorPosition(int x, int y)//sets cursor position
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void klawisz()//first Key graber
+{
+    while (true)
+    {
+        if (GetAsyncKeyState(0x36) & 0x8000)//grabing "6"
+        {
+            for (int i = 60; i >= 0; i--)
+            {
+                setCursorPosition(0, 0);//seting cursor on position 0,0
+                cout << i << endl;
+                Sleep(1000);
+            }
+        }
+    }
+}
+
+void klawisz2()//2nd key graber
+{
+    while (true)
+    {
+        if ((GetAsyncKeyState(0x2D) & 0x8000) && (GetAsyncKeyState(0x28) & 0x8000)) //checking if ins and arrow down is press if yes, then 'true'
+        {
+            insAndDownPressed = true;
+        }
+        {
+            if ((insAndDownPressed == true) && (GetAsyncKeyState(0x43) & 0x8000))//checking if down&ins is true, and if 'c' was press
+            {
+
+                for (int i = 60; i >= 0; i--)
+                {
+                    setCursorPosition(0, 1);// seting pos on 0,1
+                    cout << i << endl;
+                    Sleep(1000);
+                }
+                insAndDownPressed = false;// returing ins&down on false
+            }
+        }
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    thread t1(klawisz);//atach key graber on thread t1
+    thread t2(klawisz2);//atach 2nd key graber on thread t2
+
+    t1.detach();//detach t1 from main program
+    t2.detach();//detach t2 from main program
+
+    while (true)//it make program stay always on
+    {
+        Sleep(1000);
+    }
+    return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
